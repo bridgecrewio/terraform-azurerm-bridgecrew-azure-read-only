07ab95c448d368867947bb575b369d87bec1d0b9
Nimrod Kor <nimrodkor@gmail.com>
2020-07-06 19:32:04 +0300

    Fix secret & send it correctly

--- main.tf
+++ main.tf
@@ -59,6 +59,7 @@ resource "azurerm_role_assignment" "role_assignments" {
 resource null_resource "notify_bridgecrew" {
   triggers = {
     version = local.version
+    secret  = random_string.password.result
   }
 
   provisioner "local-exec" {
@@ -73,7 +74,7 @@ resource null_resource "notify_bridgecrew" {
                                 "subscriptionName": data.azurerm_subscription.subscription.display_name,
                                 "tenantId": data.azurerm_subscription.subscription.tenant_id,
                                 "clientId": azuread_application.bridgecrew_app.application_id,
-                                "clientSecret": azuread_service_principal_password.password.value}})}'
+                                "clientSecret": random_string.password.result}})}'
     CURL
   }
 
