75b334070aa2a3b46cb50704995c91c079dccf94
nimrodkor <nimrodkor@gmail.com>
2022-05-01 14:15:34 +0300

    Revert "Revert "Add custom read actions to extend coverage""
    This reverts commit 09f3e46923903fc36c8a4890f608a0a2d48a0389.

---
+++ azurerm_rold_definition.tf
@@ -0,0 +1,15 @@
+resource "azurerm_role_definition" "extra_read_permissions" {
+  name  = "bridgecrew-extra-read-permissions"
+  scope = data.azurerm_subscription.subscription.id
+
+  permissions {
+    actions = [
+      "Microsoft.Storage/storageAccounts/listKeys/action",
+    ]
+    not_actions = []
+  }
+
+  assignable_scopes = [
+    data.azurerm_subscription.subscription.id
+  ]
+}
---
+++ azurerm_role_assignment.tf
@@ -0,0 +1,13 @@
+resource "azurerm_role_assignment" "role_assignments" {
+  count                = length(local.roles_to_assign)
+  scope                = data.azurerm_subscription.subscription.id
+  role_definition_name = local.roles_to_assign[count.index]
+  principal_id         = azuread_service_principal.bridgecrew_sp.object_id
+}
+
+resource "azurerm_role_assignment" "extra_read_assignment" {
+  name               = "Bridgecrew"
+  principal_id       = azuread_service_principal.bridgecrew_sp.object_id
+  scope              = data.azurerm_subscription.subscription.id
+  role_definition_id = azurerm_role_definition.extra_read_permissions.role_definition_resource_id
+}
